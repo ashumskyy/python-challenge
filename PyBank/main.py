91a1da4ff663dca9905baf3ab065db1fbba0076d
ashumskyy <74363130+ashumskyy@users.noreply.github.com>
2022-12-28 22:59:57 -0500

    Python Challenge Done

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,51 +1,60 @@
-#importing modules
+# importing modules
 import os
 import csv
 
-#declaring path to the resources
+# declaring path to the resources
 budget_path = os.path.join("Resources/budget_data.csv")
 
-#declaring needed variables
+# declaring needed variables
 mon_list = []
 total_amnt_list = []
 all_chngs_list = []
 
-
-
-#reading the csv file from the path
+# reading the csv file from the path
 with open (budget_path, 'r') as csv_budget:
     budget_reader = csv.reader(csv_budget, delimiter=",")
     
-    
+    # skip the first row (the header)
     next(budget_reader)
     
+    # for loop for each row
     for row in budget_reader:
         
-        #adding months to our list
-        
+        # adding all the months to our list
         mon_list.append(row[0])
         
-        #list of net total amount
+        # adding all the net total amounts to our list
         total_amnt_list.append(int(row[1]))
         
-        #lsit with all changes in Profit/Losses
+    # for loop through total amount list 
+    # len - 1 so we won't get out of the range when form i+1 we subtract i
+    # append all the changes to a list
     for i in range(len(total_amnt_list)-1):
         all_chngs_list.append(total_amnt_list[i + 1]-total_amnt_list[i])
    
+    # find the average changes, round it to 2 decimasls
     average_chngs = round(sum(all_chngs_list)/len(all_chngs_list),2)
     
+    # find the greatest increase in profit
     grt_incr = max(all_chngs_list)
+    
+    # find the greatest decrease in profit
     grt_decr = min(all_chngs_list)
     
+    # total number of months
     ttl_months = len(mon_list)
+    
+    # find the net total amount of "Profit/Losses"
     net_ttl = sum(total_amnt_list)
      
     
-    #searching for the index we need to find correct month of the the greatest increase and decrease
+    # looking for the index value that we need so we can find the correct month of the 
+    # the greatest increase and decrease in profit
+    # since our count of months is one more than the count of differences we have to +1 to the index
     incr_month_index = all_chngs_list.index(grt_incr)+1
     decr_month_index = all_chngs_list.index(grt_decr)+1
    
-#printing the result to the terminal
+# print the result to the terminal
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {ttl_months}")
@@ -54,9 +63,10 @@ print(f"Average Change: ${average_chngs}")
 print(f"Greatest Increase in Profits: {mon_list[incr_month_index]} (${grt_incr})")
 print(f"Greatest Decrease in Profits: {mon_list[decr_month_index]} (${grt_decr})")
 
-
+# path for .txt file
 budget_output_path = os.path.join("analysis/budget_data.txt")
 
+# writing a .txt file
 with open(budget_output_path, 'w') as budget_output:
     budger_writer = csv.writer(budget_output)    
     
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,53 +11,70 @@ ttl_num_votes = 0
 candidats_list = []
 candidats_dict = {}
 
+# read the csv file
 with open (election_path, 'r') as csv_election:
     election_reader = csv.reader(csv_election, delimiter=",")
     
+    # skip the first row (the header)
     next(election_reader)
     
+    # for loop through the entire csv file to find needed values
     for row in election_reader:
+        
+        # counting total number of the votes
         ttl_num_votes += 1
         
         # creat a var for candidates name
         cand_name = row[2]
         
         # if the candidates not in the list
-        
         if cand_name not in candidats_list:
             
             # add it
-            
             candidats_list.append(cand_name)
             
             # tracking votes
-            
             candidats_dict[cand_name] = 0
             
-
-
         # count candidates 
-
         candidats_dict[cand_name] = candidats_dict[cand_name] + 1
 
-# creat election results
 
+    # empty percentage list where we will append the result
+    percentage_list = []
+
+    # for loop to calculate precentage of the votes for each candidate
+    for value in candidats_dict.values():
+        percentage_list.append((value*100)/ttl_num_votes)
+
+# a list of the keys from the dictionary to refer to the values
+keys = list(candidats_dict)
+
+# find the winner from our dictionary based on the max value
+winner = max(candidats_dict, key=candidats_dict.get)
+
+# creat election results
 election_results = (
     f"Election Results\n"
     f"-------------------------\n"
     f"Total Votes: {ttl_num_votes}\n"
     f"-------------------------\n"
+    f"{candidats_list[0]}: {round(percentage_list[0], 3)}% ({candidats_dict[keys[0]]})\n"
+    f"{candidats_list[1]}: {round(percentage_list[1], 3)}% ({candidats_dict[keys[1]]})\n"
+    f"{candidats_list[2]}: {round(percentage_list[2], 3)}% ({candidats_dict[keys[2]]})\n"
+    f"-------------------------\n"
+    f"Winner: {winner}\n"
+    f"-------------------------\n"
 )
 
-print(election_results)      
-print(candidats_list)
-print(candidats_dict)
+# print the result to the terminal
+print(election_results)
 
 
+# write the result to .txt file
 election_path = os.path.join("analysis/election_data.txt")
 
 with open (election_path, 'w') as text_file:
-    # election_writer = csv.writer(election_output)
     
     text_file.write(election_results)
     
